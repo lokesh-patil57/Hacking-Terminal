86cfb23772e1ca5b76ea6c325dee5f52ffc00c4e
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-21 03:16:28 +0530

    Hacking Completed

--- Hacking Terminal/app.js
+++ Hacking Terminal/app.js
@@ -10,18 +10,33 @@ let text = [
 
 const addItem = async (item) => {
   await ranDelay();
-  let div = document.createElement()
+  let div = document.createElement("div");
+  div.innerHTML = item;
+  document.body.append(div);
 };
 
 const ranDelay = () => {
   return new Promise((res, rej) => {
-    let timeOut = Math.floor(Math.random() * 5) + 1;
+    let timeOut = Math.floor(Math.random() * 3) + 1;
     setTimeout(() => {
-      res()
-    }, timeout*1000); 
+      res();
+    }, timeOut * 1000);
   });
 };
+async function main() {
+  let t = setInterval(() => {
+    let last = document.body.getElementsByTagName("div");
+    last = last[last.length - 1];
+    if (last.innerHTML.endsWith("...")) {
+      last.innerHTML = last.innerHTML.slice(0, last.innerHTML.length - 3);
+    } else {
+      last.innerHTML = last.innerHTML + ".";
+    }
+  }, 150);
 
-for (const val of text) {
-  addItem(item);
+  for (const item of text) {
+    await addItem(item);
+  }
 }
+
+main();
